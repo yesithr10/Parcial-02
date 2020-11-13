1c70356ae6183d473f1e1ec5b472e2e1b471a9da
Orlando Rojano <orlandorojano@hotmail.com>
2020-11-12 20:09:32 -0500

    froms

--- parcial2/ClientApp/src/app/app-routing.module.ts
+++ parcial2/ClientApp/src/app/app-routing.module.ts
@@ -1,12 +1,38 @@
+import { TerceroRegistroComponent } from './Parcial2/tercero-registro/tercero-registro.component';
+import { TerceroConsultaComponent } from './Parcial2/tercero-consulta/tercero-consulta.component';
+import { PagoConsultaComponent } from './Parcial2/pago-consulta/pago-consulta.component';
+import { PagoRegistroComponent } from './Parcial2/pago-registro/pago-registro.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { from } from 'rxjs';
+import { Routes, RouterModule } from '@angular/router';
 
-
+const routes: Routes = [
+  {
+  path: 'terceroRegistro',
+  component: TerceroRegistroComponent
+  },
+  {
+  path: 'terceroConsulta',
+  component: TerceroConsultaComponent
+  },
+  {
+  path: 'pagoRegistro',
+  component: PagoRegistroComponent
+  },
+  {
+  path: 'pagoConsulta',
+  component: PagoConsultaComponent
+  }
+  ];
+  
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    RouterModule.forRoot(routes)
+  ],
+  exports:[RouterModule]
 })
 export class AppRoutingModule { }
--- parcial2/ClientApp/src/app/parcial2/models/tercero.ts
+++ parcial2/ClientApp/src/app/parcial2/models/tercero.ts
@@ -1,2 +1,9 @@
 export class Tercero {
+    cedula: string;
+    nombre: string;
+    direccion: string;
+    telefono: string;
+    pais: string;
+    departamento: string;
+    ciudad: string;
 }
